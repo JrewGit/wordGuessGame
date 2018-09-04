8023633bfb3fdee2bd4ed8dc8e9e74b13a790d8e
Jrew <jrewmohamed@gmail.com>
2018-09-04 12:31:40 -0400

    Fixed blank space updates, win and loss message, and letter guess array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,91 @@
+//Initiates the starting variables
 var livesLeft = 10;
+var win = false;
 var userLetterGuess = [];
 var blankSpace = [];
 var wordList = ["Friends", "The Fresh Prince of Bel Air", "Family Matters", "Martin", "Full House", "All That", "Boy Meets World", "Saved By the Bell", "The Jamie Foxx Show", "Everybody Loves Raymond"];
 var computerChoice = Math.floor(Math.random() * wordList.length);
 var computerGuess = wordList[computerChoice];
+var uppercaseComputerGuess = computerGuess.toUpperCase();
+var lowercaseComputerGuess = computerGuess.toLowerCase();
 
+//Grabs the HTML and connects the javascript
+var updateWinStatement = document.getElementById("winStatement");
+var updateLossStatement = document.getElementById("lossStatement");
 var updateLives = document.getElementById("lives");
 var updateLettersGuessed = document.getElementById("lettersGuessed");
 var updateBlanks = document.getElementById("blanks");
 updateBlanks.innerHTML = makeBlankSpace();
 updateLives.textContent = livesLeft;
 
+//resets the game
 function reset() {
-    livesLeft = 0;
+    livesLeft = 10;
+    updateLives.textContent = livesLeft;
+    win = false;
     userLetterGuess = [];
+    updateLettersGuessed.innerHTML = userLetterGuess;
+    blankSpace = [];
+    updateBlanks.innerHTML = makeBlankSpace();
     computerChoice = Math.floor(Math.random() * wordList.length);
     computerGuess = wordList[computerChoice];
+    console.log(computerGuess);
 }
 
+//prints the blank spaces on the screen as soon as the document is shown.
 function makeBlankSpace() {
     for (var i = 0; i < computerGuess.length; i++) {
-        // blankSpace.push("_");
         if (computerGuess[i] === " ") {
-            blankSpace.push(" ");
+            blankSpace.push("-");
         } else {
             blankSpace.push("_");
-        }
-  };
-    return blankSpace.join(',');
-}
+        };
+    };
+    return blankSpace.join(" ");
+};
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
-    
-    if (userGuess === computerGuess.toLowerCase()[0] || userGuess === computerGuess.toUpperCase()[0]) {
-        blankSpace[0] = computerGuess[0];
-        updateBlanks.textContent = blankSpace;
-    }
+
+    //updates the array of letters that the user guessed so far.
+    if ((userLetterGuess.indexOf(userGuess) === -1) && (livesLeft > 0) && (win === false)) {
+        userLetterGuess.push(userGuess);
+        updateLettersGuessed.textContent = userLetterGuess;
+    };
+
+    //Checks to see if the letter that the user guesses is in the comptuer's guess. If it is, then the letter that the user guessed fills in the necessary blanks.
+    for (var i = 0; i < computerGuess.length; i++) {
+        if ((userGuess === lowercaseComputerGuess[i] || userGuess === uppercaseComputerGuess[i]) && userGuess !== " ") {
+            blankSpace[i] = computerGuess[i];
+            updateBlanks.textContent = blankSpace.join(" ");
+        };
+    };
+
+    //Decrements the lives left if the letter that the user guesses isn't a letter in the computer's guess.
+    if ((lowercaseComputerGuess.indexOf(userGuess) === -1) && (livesLeft > 0) && (wins = false)) {
+        livesLeft -= 1;
+        updateLives.textContent = livesLeft;
+    };
+
+    //Prints loss statement if lives === 0.
+    if (livesLeft === 0) {
+        updateLossStatement.textContent = "Oooo, so close! Hit 'Space' to try again";
+        if (userGuess === " ") {
+            reset();
+        }
+    };
+
+    //Prints win statement if there are no more blank spaces.
+    if (blankSpace.indexOf("_") === -1) {
+        win = true;
+        updateWinStatement.textContent = "You did it! Hit 'Space' to play again";
+        if (userGuess === " ") {
+            reset();
+        }
+    };
+
+    console.log(userGuess);
 
 };
 
